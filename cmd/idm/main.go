94b6ca41fe3f0d32aa7aeb531369cc056914bf2d
Lei Wang <tendant@gmail.com>
2024-05-13 11:48:19 -0700

    add /hello

--- cmd/idm/main.go
+++ cmd/idm/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
+	"github.com/tendant/chi-demo/app"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	myApp := app.Default()
+	Routes(myApp.R)
+	myApp.Run()
+
+}
+
+func Routes(r *chi.Mux) {
+	r.Get("/hello", func(w http.ResponseWriter, r *http.Request) {
+		render.PlainText(w, r, http.StatusText(http.StatusOK))
+	})
+
 }
