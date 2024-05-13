3ecc11aead18511b4eb1c56cfa30ab3cbda92cdf
Lei Wang <tendant@gmail.com>
2024-05-13 12:27:23 -0700

    add params

--- cmd/idm/main.go
+++ cmd/idm/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/ggicci/httpin"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/render"
 	"github.com/tendant/chi-demo/app"
@@ -20,7 +21,11 @@ func Routes(r *chi.Mux) {
 		render.PlainText(w, r, http.StatusText(http.StatusOK))
 	})
 
-	r.Post("/api/users", handleCreateUser)
+	r.With(httpin.NewInput(UserParams{})).Post("/api/users", handleCreateUser)
+}
+
+type UserParams struct {
+	Email string
 }
 
 func handleCreateUser(w http.ResponseWriter, r *http.Request) {
