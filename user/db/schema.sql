cda2df38b50244f02d2d047d7dc0a18897a5b782
Lei Wang <tendant@gmail.com>
2024-05-13 13:26:09 -0700

    add schema & sqlc

---
+++ user/db/schema.sql
@@ -0,0 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users (
+    uuid uuid DEFAULT uuid_generate_v4() NOT NULL,
+    created_at timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc'::text) NOT NULL,
+    last_modified_at timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc'::text) NOT NULL,
+    deleted_at timestamp without time zone,
+    created_by character varying(255),
+    email character varying(255) NOT NULL
+);
