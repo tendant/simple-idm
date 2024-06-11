21247081b69baf6569eb5845b5b9a78e6ab9f4f8
apan09 <apan.lunar@gmail.com>
2024-06-11 13:47:10 -0700

    demo: update script

--- demoService/goapi-gen.sh
+++ demoService/goapi-gen.sh
@@ -3,4 +3,4 @@ echo "auto gen demo.gen.go at demo"
 # -o output file location
 # -p expected package name
 # yaml file
-goapi-gen -o demo.gen.go -p demo demo.yaml
+goapi-gen -o demo.gen.go -p demoService demo.yaml
