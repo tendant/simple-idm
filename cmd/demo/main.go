e0b0f4ff4cddd9f78e4d45ec0da10affdb85eae7
apan09 <apan.lunar@gmail.com>
2024-06-11 13:48:49 -0700

    demo: mount routes

--- cmd/demo/main.go
+++ cmd/demo/main.go
@@ -1,9 +1,15 @@
 package main
 
-import "github.com/tendant/chi-demo/app"
+import (
+	"github.com/tendant/chi-demo/app"
+	"github.com/tendant/simple-user/demoService"
+)
 
 func main() {
 	myApp := app.Default()
+
+	handler := demoService.Handle{}
+	demoService.Routes(myApp.R, handler)
 
 	myApp.Run()
 }
