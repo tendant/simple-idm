4705da5aab306a2ba78436648ce953f4f82e271d
apan09 <apan.lunar@gmail.com>
2024-06-12 15:15:18 -0700

    login: add route

---
+++ login/route.go
@@ -0,0 +1,26 @@
+package login
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type Handle struct {
+	loginService *LoginService
+}
+
+func Routes(r *chi.Mux, handle Handle) {
+
+	r.Group(func(r chi.Router) {
+		// add auth middleware
+		r.Mount("/api/v4", Handler(&handle))
+	})
+}
+
+func (h Handle) PostLogin(w http.ResponseWriter, r *http.Request) *Response {
+	return &Response{
+		Code: http.StatusNotImplemented,
+	}
+
+}
