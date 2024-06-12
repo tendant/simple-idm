b022c5848b6db3a42bc9475b4314e34f5fb49aab
xiwencao <xc458@cornell.edu>
2024-06-12 15:06:42 -0700

    Add sql for GetUserUUID

--- user/db/query.sql
+++ user/db/query.sql
@@ -13,3 +13,8 @@ limit 20;
 -- name: UpdateUser :one
 UPDATE users SET name = $2 WHERE uuid = $1
 RETURNING *;
+
+-- name: GetUserUUID :one
+SELECT uuid, created_at, last_modified_at, deleted_at, created_by, email, name
+FROM users
+WHERE uuid = $1;
