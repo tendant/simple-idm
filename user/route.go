65414d798df49301b304678b2c9f038b88e4f608
demo <Demo@Leis-MacBook-Pro.local>
2024-06-12 10:53:45 -0700

    add route.go

---
+++ user/route.go
@@ -0,0 +1,48 @@
+package user
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type Handle struct {
+}
+
+// Get a list of users
+// (GET /user)
+func (h Handle) GetUser(w http.ResponseWriter, r *http.Request) *Response {
+	return nil
+}
+
+// Create a new user
+// (POST /user)
+func (h Handle) PostUser(w http.ResponseWriter, r *http.Request) *Response {
+	return nil
+}
+
+// Delete user by UUID
+// (DELETE /user/{uuid})
+func (h Handle) DeleteUserUUID(w http.ResponseWriter, r *http.Request, uuid string) *Response {
+	return nil
+}
+
+// Get user details by UUID
+// (GET /user/{uuid})
+func (h Handle) GetUserUUID(w http.ResponseWriter, r *http.Request, uuid string) *Response {
+	return nil
+}
+
+// Update user details by UUID
+// (PUT /user/{uuid})
+func (h Handle) PutUserUUID(w http.ResponseWriter, r *http.Request, uuid string) *Response {
+	return nil
+}
+
+func Routes(r *chi.Mux, handle Handle) {
+
+	r.Group(func(r chi.Router) {
+		// add auth middleware
+		r.Mount("/api/v4", Handler(&handle))
+	})
+}
