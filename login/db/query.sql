338975c758ef4aa68976ab8125534c73df2f24c8
charlene0122 <zhifeiwu@hotmail.com>
2024-06-13 14:56:12 -0700

    update query.sql

--- login/db/query.sql
+++ login/db/query.sql
@@ -2,3 +2,10 @@
 SELECT uuid, created_at, last_modified_at, deleted_at, created_by, email, name
 FROM users
 limit 20;
+
+
+-- name: ResetPassword :exec
+UPDATE users
+SET password = $1, 
+    last_modified_at = NOW()
+WHERE email = $2;  
