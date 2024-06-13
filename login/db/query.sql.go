2c6da6e157ee1899d02cfc4d2f96f64db3253c8e
charlene0122 <zhifeiwu@hotmail.com>
2024-06-13 15:30:20 -0700

    sqlc generate

--- login/db/query.sql.go
+++ login/db/query.sql.go
@@ -7,6 +7,10 @@ package db
 
 import (
 	"context"
+	"database/sql"
+	"time"
+
+	"github.com/google/uuid"
 )
 
 const findUsers = `-- name: FindUsers :many
@@ -15,15 +19,25 @@ FROM users
 limit 20
 `
 
-func (q *Queries) FindUsers(ctx context.Context) ([]User, error) {
+type FindUsersRow struct {
+	Uuid           uuid.UUID      `json:"uuid"`
+	CreatedAt      time.Time      `json:"created_at"`
+	LastModifiedAt time.Time      `json:"last_modified_at"`
+	DeletedAt      sql.NullTime   `json:"deleted_at"`
+	CreatedBy      sql.NullString `json:"created_by"`
+	Email          string         `json:"email"`
+	Name           sql.NullString `json:"name"`
+}
+
+func (q *Queries) FindUsers(ctx context.Context) ([]FindUsersRow, error) {
 	rows, err := q.db.Query(ctx, findUsers)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
-	var items []User
+	var items []FindUsersRow
 	for rows.Next() {
-		var i User
+		var i FindUsersRow
 		if err := rows.Scan(
 			&i.Uuid,
 			&i.CreatedAt,
@@ -41,4 +55,21 @@ func (q *Queries) FindUsers(ctx context.Context) ([]User, error) {
 		return nil, err
 	}
 	return items, nil
+}
+
+const resetPassword = `-- name: ResetPassword :exec
+UPDATE users
+SET password = $1, 
+    last_modified_at = NOW()
+WHERE email = $2
+`
+
+type ResetPasswordParams struct {
+	Password []byte `json:"password"`
+	Email    string `json:"email"`
+}
+
+func (q *Queries) ResetPassword(ctx context.Context, arg ResetPasswordParams) error {
+	_, err := q.db.Exec(ctx, resetPassword, arg.Password, arg.Email)
+	return err
 }
