b0ac9d68e19e71e70def05c3216abd282172863d
Jimmy Pan <jimmy@torpago.com>
2024-06-13 16:53:57 -0700

    add login query

--- login/login.go
+++ login/login.go
@@ -1,6 +1,8 @@
 package login
 
 import (
+	"context"
+
 	"github.com/tendant/simple-user/login/db"
 )
 
@@ -12,4 +14,13 @@ func New(queries *db.Queries) *LoginService {
 	return &LoginService{
 		queries: queries,
 	}
+}
+
+type LoginParams struct {
+	Email string
+}
+
+func (s LoginService) Login(ctx context.Context, params LoginParams) (db.FindUserRow, error) {
+	user, err := s.queries.FindUser(ctx, params.Email)
+	return user, err
 }
