a5a9cb12e82539f07f1d1e9e1a0ee44b1ae1bf8a
Shuoze Li <szsuzyli@gmail.com>
2024-06-13 22:19:04 -0700

    add sql for user registration

--- login/db/query.sql
+++ login/db/query.sql
@@ -2,3 +2,8 @@
 SELECT uuid, created_at, last_modified_at, deleted_at, created_by, email, name
 FROM users
 limit 20;
+
+-- name: RegisterUser :one
+INSERT INTO users (email, name, password, created_at)
+VALUES ($1, $2, $3, CURRENT_TIMESTAMP)
+RETURNING *;
