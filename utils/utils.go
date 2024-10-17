8b83b2e274916c15f6b28177886d90be4826f358
AustinZhouOWO <73068151+AustinZhouOWO@users.noreply.github.com>
2024-10-17 10:42:36 -0700

    feat: add utils

---
+++ utils/utils.go
@@ -0,0 +1,16 @@
+package utils
+
+import "database/sql"
+
+func ToNullString(str string) sql.NullString {
+	if str == "" {
+		return sql.NullString{
+			String: str,
+			Valid:  false,
+		}
+	}
+	return sql.NullString{
+		String: str,
+		Valid:  true,
+	}
+}
