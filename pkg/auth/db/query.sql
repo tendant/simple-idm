490bf74fbf451e279a5805d973156a0dc34f7126
AustinZhouOWO <73068151+AustinZhouOWO@users.noreply.github.com>
2024-11-07 18:24:48 -0800

    feat: update

--- pkg/auth/db/query.sql
+++ pkg/auth/db/query.sql
@@ -1,4 +1,11 @@
 -- name: FindUserByUserUuid :one
 SELECT users.uuid, name, username, email, password
 FROM users
-WHERE uuid = $1;
+WHERE uuid = $1
+AND deleted_at IS NULL;
+
+-- name: UpdatePassowrd :exec
+UPDATE users
+SET password = $1, last_modified_at = $2
+WHERE uuid = $3
+AND deleted_at IS NULL;
