ace483851dc8492f209a378186b3e8c02b68c3bf
Lei Wang <tendant@gmail.com>
2025-01-21 05:56:35 -0800

    fix route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,18 +9,67 @@ import CreateRole from './pages/CreateRole';
 import EditRole from './pages/EditRole';
 import Navigation from './components/Navigation';
 
-const ProtectedRoutes: Component = () => {
+const UsersPage: Component = () => {
   return (
     <div class="min-h-screen bg-gray-1">
-      <Navigation />
       <main class="py-10 px-4 sm:px-6 lg:px-8">
-        <Route path="/" component={Users} />
-        <Route path="/users" component={Users} />
-        <Route path="/users/create" component={CreateUser} />
-        <Route path="/users/:id/edit" component={EditUser} />
-        <Route path="/roles" component={Roles} />
-        <Route path="/roles/create" component={CreateRole} />
-        <Route path="/roles/:uuid/edit" component={EditRole} />
+        <Navigation />
+        <Users />
+      </main>
+    </div>
+  );
+};
+
+const CreateUserPage: Component = () => {
+  return (
+    <div class="min-h-screen bg-gray-1">
+      <main class="py-10 px-4 sm:px-6 lg:px-8">
+        <Navigation />
+        <CreateUser />
+      </main>
+    </div>
+  );
+};
+
+const EditUserPage: Component = () => {
+  return (
+    <div class="min-h-screen bg-gray-1">
+      <main class="py-10 px-4 sm:px-6 lg:px-8">
+        <Navigation />
+        <EditUser />
+      </main>
+    </div>
+  );
+};
+
+const RolesPage: Component = () => {
+  return (
+    <div class="min-h-screen bg-gray-1">
+      <main class="py-10 px-4 sm:px-6 lg:px-8">
+        <Navigation />
+        <Roles />
+      </main>
+    </div>
+  );
+};
+
+const CreateRolePage: Component = () => {
+  return (
+    <div class="min-h-screen bg-gray-1">
+      <main class="py-10 px-4 sm:px-6 lg:px-8">
+        <Navigation />
+        <CreateRole />
+      </main>
+    </div>
+  );
+};
+
+const EditRolePage: Component = () => {
+  return (
+    <div class="min-h-screen bg-gray-1">
+      <main class="py-10 px-4 sm:px-6 lg:px-8">
+        <Navigation />
+        <EditRole />
       </main>
     </div>
   );
@@ -30,7 +79,13 @@ const App: Component = () => {
   return (
     <Router>
       <Route path="/login" component={Login} />
-      <Route path="/*" component={ProtectedRoutes} />
+      <Route path="/" component={UsersPage} />
+      <Route path="/users" component={UsersPage} />
+      <Route path="/users/create" component={CreateUserPage} />
+      <Route path="/users/:id/edit" component={EditUserPage} />
+      <Route path="/roles" component={RolesPage} />
+      <Route path="/roles/create" component={CreateRolePage} />
+      <Route path="/roles/:uuid/edit" component={EditRolePage} />
     </Router>
   );
 };
