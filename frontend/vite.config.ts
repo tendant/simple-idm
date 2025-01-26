311c9f977c7d602dab4e1b1645e683f3afa0c8c2
Lei Wang <tendant@gmail.com>
2025-01-25 22:35:20 -0800

    add solid-ui

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import tailwindcss from '@tailwindcss/vite';
 
+import path from "path";
+
 export default defineConfig({
   plugins: [
     solidPlugin(),
@@ -25,4 +27,9 @@ export default defineConfig({
   build: {
     target: 'esnext',
   },
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./src")
+    }
+  }
 });
