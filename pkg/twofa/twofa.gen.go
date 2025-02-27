50b8c385f61ab11a408fd87058e4acbf481487d0
Shuozeli <szsuzyli@gmail.com>
2025-02-27 11:32:47 -0800

    goapi generate

--- pkg/twofa/twofa.gen.go
+++ pkg/twofa/twofa.gen.go
@@ -15,6 +15,7 @@ import (
 	"path"
 	"strings"
 
+	"github.com/discord-gophers/goapi-gen/runtime"
 	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/render"
@@ -116,6 +117,24 @@ func Post2faInitJSON200Response(body SuccessResponse) *Response {
 	}
 }
 
+// Get2faEnabledJSON200Response is a constructor method for a Get2faEnabled response.
+// A *Response is returned with the configured status code and content type from the spec.
+func Get2faEnabledJSON200Response(body struct {
+	N2faMethods []struct {
+		// The type of 2FA method
+		Type string `json:"type"`
+	} `json:"2fa_methods"`
+
+	// Number of enabled 2FA methods
+	Count int `json:"count"`
+}) *Response {
+	return &Response{
+		body:        body,
+		Code:        200,
+		contentType: "application/json",
+	}
+}
+
 // ServerInterface represents all server handlers.
 type ServerInterface interface {
 	// Authenticate 2fa passcode
@@ -124,6 +143,9 @@ type ServerInterface interface {
 	// Initiate sending 2fa code
 	// (POST /2fa:init)
 	Post2faInit(w http.ResponseWriter, r *http.Request) *Response
+	// Get all enabled 2fas
+	// (GET /{login_id}/2fa/enabled)
+	Get2faEnabled(w http.ResponseWriter, r *http.Request, loginID string) *Response
 }
 
 // ServerInterfaceWrapper converts contexts to parameters.
@@ -156,6 +178,32 @@ func (siw *ServerInterfaceWrapper) Post2faInit(w http.ResponseWriter, r *http.Re
 
 	var handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		resp := siw.Handler.Post2faInit(w, r)
+		if resp != nil {
+			if resp.body != nil {
+				render.Render(w, r, resp)
+			} else {
+				w.WriteHeader(resp.Code)
+			}
+		}
+	})
+
+	handler(w, r.WithContext(ctx))
+}
+
+// Get2faEnabled operation middleware
+func (siw *ServerInterfaceWrapper) Get2faEnabled(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	// ------------- Path parameter "login_id" -------------
+	var loginID string
+
+	if err := runtime.BindStyledParameter("simple", false, "login_id", chi.URLParam(r, "login_id"), &loginID); err != nil {
+		siw.ErrorHandlerFunc(w, r, &InvalidParamFormatError{err, "login_id"})
+		return
+	}
+
+	var handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		resp := siw.Handler.Get2faEnabled(w, r, loginID)
 		if resp != nil {
 			if resp.body != nil {
 				render.Render(w, r, resp)
@@ -285,6 +333,7 @@ func Handler(si ServerInterface, opts ...ServerOption) http.Handler {
 	r.Route(options.BaseURL, func(r chi.Router) {
 		r.Post("/2fa", wrapper.Post2faValidate)
 		r.Post("/2fa:init", wrapper.Post2faInit)
+		r.Get("/{login_id}/2fa/enabled", wrapper.Get2faEnabled)
 	})
 	return r
 }
@@ -310,14 +359,17 @@ func WithErrorHandler(handler func(w http.ResponseWriter, r *http.Request, err e
 // Base64 encoded, gzipped, json marshaled Swagger object
 var swaggerSpec = []string{
 
-	"H4sIAAAAAAAC/9SSQY/TMBCF/0o0cEw3pdx8W245IK1A4oLQysSTdFaJx3jGhWqV/47sppSWClSJy96s",
-	"0bzxm/fNM3Q8BfboVcA8g3RbnGx5fkxdhyIfUAJ7wVwKkQNGJSwNESWNml+6DwgGRCP5Aea5Plb46xN2",
-	"CjX8WA284qDE3o6rnR0TgtGYcM7d5Hsuc0jHLGungFHYW8XqvfV2wAm9VvcPLdSwwyjEHgy8uVvfrWGu",
-	"gQN6GwgMvC2lGoLVbTHZbHpbrLMUq3kBm220Dgw8sOimt5/sSM4qQg0RvyUUfcdun9s79oq+KG0II3VF",
-	"2zxJNnCM689oRh7IP5LL757jZBUMpEQO6suwsleRjh1eSbIG/c69fTyUrwWd/VJEB+bzaVB9MnA24csl",
-	"mPl8RAGSCwfkZZXNen1TEK8j9mDgVXM6rGa5qubypMr3DqWLVE4DDGx6W+0WHK5aBH0ay7KSpsnGPRi4",
-	"T7pFr9kGVlnza/fcl6Eb8qT/JN/mpv9FHSdL41WKN93DLcgPX55pfvvthfDOpOjvvNulpRL0jvxQmC+8",
-	"5/lnAAAA//83ZIOaxgQAAA==",
+	"H4sIAAAAAAAC/9RUT2/TThD9Ktb8fke3CeG2tyJBlQOoAsSlqqqpPXa22j9mdzYQRf7uaDZJHScGFMGF",
+	"mzN5M/PmvWdvofK2844cR1BbiNWKLObHT6mqKMaPFDvvIkmpC76jwJoyIFBMhuWJNx2BgshBuxb6vjxU",
+	"/NMzVQwlfL9q/ZXvWHuH5mqNJhEoDol6QWvX+DxHs5G2pe0oRO+QqXiPDluy5Li4uVtCCWsKUXsHCl5d",
+	"z6/n0JfgO3LYaVDwOpdK6JBXmeRs0WCm7mOmKgeg0FjWoODOR140+AWNrpEJSgj0NVHkN77eCLzyjsnl",
+	"Tuw6o6vcO3uOQuAg17k0xrfaPepanhsfLDIoSEnXUJ6KJVxjrHxNE0qWwN98g4+78pTQwlcHqkHdD4PK",
+	"gcBowsOpMf14RDZECjvL8ymL+fwiIf4P1ICC/2ZDsGb7VM1OI5XX1xSroHM0QMGiwWK9t6Mu9g1NMvnY",
+	"mKzFsAEFN4lX5FhoUCE9L7cLTkxX2mn+rfNLAf0t18miNpMuXpSHSyzfrRz1HG37R/wWp/Sv/V7uIUUk",
+	"V2vXZs8Hv7eHk3uxfkYOnwxltVuaMP+WxPu3e5S8gQEtMYUI6n4LWojJFwRKcGhFv6P3aSxfeSTFqVUP",
+	"fyjtOF2LBh8t8crX+admsvEcdYjNWOXPKyrkn8I3xeLdTbGbcx6+k3T95KPxUsAQcCO/K592B43Xfkj2",
+	"iYIs3TtytDwO27Vjaimcrd9NLUeXT0d6vHZIkdkUgThoWlM9yWGcslviAo0ZkA0KpO9/BAAA//8oDJKG",
+	"JwcAAA==",
 }
 
 // GetSwagger returns the content of the embedded swagger specification file
