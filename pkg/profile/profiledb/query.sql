209778df1cce1fb595e6f3fc9d5a15be9d315b14
charlene0122 <zhifeiwu@hotmail.com>
2025-02-28 15:47:41 -0800

    fix query and add GetLoginIDByUserID query

--- pkg/profile/profiledb/query.sql
+++ pkg/profile/profiledb/query.sql
@@ -2,21 +2,21 @@
 UPDATE users
 SET username = $2,
     last_modified_at = NOW()
-WHERE uuid = $1;
+WHERE id = $1;
 
 -- name: UpdateUserPassword :exec
 UPDATE users
 SET password = $2,
     last_modified_at = NOW()
-WHERE uuid = $1;  -- Verify current password
+WHERE id = $1;  -- Verify current password
 
--- name: GetUserByUUID :one
-SELECT uuid, username, email, password, created_at, last_modified_at
+-- name: GetUserById :one
+SELECT id, username, email, password, created_at, last_modified_at
 FROM users
-WHERE uuid = $1;
+WHERE id = $1;
 
 -- name: FindUserByUsername :many
-SELECT uuid, username, email, password, two_factor_secret, two_factor_enabled, two_factor_backup_codes, created_at, last_modified_at
+SELECT id, username, email, password, two_factor_secret, two_factor_enabled, two_factor_backup_codes, created_at, last_modified_at
 FROM users
 WHERE username = $1;
 
@@ -26,18 +26,18 @@ SET two_factor_secret = NULL,
     two_factor_enabled = FALSE,
     two_factor_backup_codes = NULL,
     last_modified_at = NOW()
-WHERE uuid = $1;
+WHERE id = $1;
 
--- name: Disable2FAByLoginUuid :exec
+-- name: Disable2FAByLoginId :exec
 UPDATE login_2fa
 SET deleted_at = now() AT TIME ZONE 'utc'
-WHERE login_uuid = $1
+WHERE login_id = $1
 AND deleted_at IS NULL;
 
 -- name: Create2FAInit :one
-INSERT INTO login_2fa (login_uuid, two_factor_secret, two_factor_enabled, two_factor_backup_codes)
+INSERT INTO login_2fa (login_id, two_factor_secret, two_factor_enabled, two_factor_backup_codes)
 VALUES ($1, $2, FALSE, $3::TEXT[])
-RETURNING uuid;
+RETURNING id;
 
 -- name: Enable2FA :exec
 UPDATE users
@@ -45,13 +45,20 @@ SET two_factor_secret = $1::text,
     two_factor_enabled = TRUE,
     two_factor_backup_codes = $2::text[],
     last_modified_at = NOW()
-WHERE uuid = $3;
+WHERE id = $3;
 
--- name: Enable2FAByLoginUuid :exec
+-- name: Enable2FAByLoginId :exec
 UPDATE login_2fa
 SET two_factor_secret = $1::text,
     two_factor_enabled = TRUE,
     two_factor_backup_codes = $2::text[],
     last_modified_at = NOW()
-WHERE login_uuid = $3
+WHERE login_id = $3
 AND deleted_at IS NULL;
+
+-- name: GetLoginIDByUserID :one
+SELECT l.id
+FROM login l
+JOIN users u ON l.id = u.login_id
+WHERE u.id = $1
+AND l.deleted_at IS NULL;
