6aacd2b5f5b2d0dfe1afee7acb04ae117a5ae113
ivanlee1999 <liyian3829@outlook.com>
2025-03-14 14:17:48 -0700

    refactor: Update UserRepository to mapper.UserRepository

--- pkg/login/repository.go
+++ pkg/login/repository.go
@@ -129,7 +129,7 @@ type PostgresLoginRepository struct {
 // while hiding the pgx implementation details
 type PostgresLoginRepositoryAdapter interface {
 	LoginRepository
-	UserRepository
+	mapper.UserRepository
 	WithPgxTx(tx pgx.Tx) LoginRepository
 }
 
@@ -424,21 +424,21 @@ func (r *PostgresLoginRepository) FindUsersByLoginID(ctx context.Context, loginI
 	if err != nil {
 		return nil, err
 	}
-	
+
 	// Convert UserWithRoles to mapper.User
 	users := make([]mapper.User, len(usersWithRoles))
 	for i, userWithRoles := range usersWithRoles {
 		// Create ExtraClaims map
 		extraClaims := make(map[string]interface{})
-		
+
 		// Add roles to ExtraClaims
 		extraClaims["roles"] = userWithRoles.Roles
-		
+
 		// Create UserInfo
 		userInfo := mapper.UserInfo{
 			Email: userWithRoles.Email,
 		}
-		
+
 		// Create User
 		users[i] = mapper.User{
 			UserID:      userWithRoles.ID.String(),
@@ -448,7 +448,7 @@ func (r *PostgresLoginRepository) FindUsersByLoginID(ctx context.Context, loginI
 			ExtraClaims: extraClaims,
 		}
 	}
-	
+
 	return users, nil
 }
 
@@ -459,24 +459,24 @@ func (r *PostgresLoginRepository) GetUserByUserID(ctx context.Context, userID uu
 	if err != nil {
 		return mapper.User{}, err
 	}
-	
+
 	// Get login information
 	loginEntity, err := r.GetLoginByUserId(ctx, userID)
 	if err != nil {
 		return mapper.User{}, err
 	}
-	
+
 	// Create ExtraClaims map
 	extraClaims := make(map[string]interface{})
-	
+
 	// Add roles to ExtraClaims
 	extraClaims["roles"] = userInfo.Roles
-	
+
 	// Create mapper.UserInfo
 	mapperUserInfo := mapper.UserInfo{
 		Email: userInfo.Email,
 	}
-	
+
 	// Create User
 	user := mapper.User{
 		UserID:      userID.String(),
@@ -485,7 +485,7 @@ func (r *PostgresLoginRepository) GetUserByUserID(ctx context.Context, userID uu
 		UserInfo:    mapperUserInfo,
 		ExtraClaims: extraClaims,
 	}
-	
+
 	return user, nil
 }
 
@@ -497,12 +497,12 @@ func (r *PostgresLoginRepository) FindUsernamesByEmail(ctx context.Context, emai
 	if err != nil {
 		return nil, err
 	}
-	
+
 	if !valid {
 		// No username found for this email
 		return []string{}, nil
 	}
-	
+
 	// Return a slice with the single username
 	return []string{username}, nil
 }
