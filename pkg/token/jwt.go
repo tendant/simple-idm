21328cc53a74666b0b6e0a6a9207dd11df4fb027
ivanlee1999 <liyian3829@outlook.com>
2025-03-18 12:29:17 -0700

    feat: Add JwtConfig struct with options and constructor

---
+++ pkg/token/jwt.go
@@ -0,0 +1,36 @@
+package token
+
+// JwtConfig holds JWT service configuration
+type JwtConfig struct {
+	Secret          string
+	CoookieHttpOnly bool
+	CookieSecure    bool
+}
+
+// JwtOption defines a function type for configuring JwtConfig
+type JwtOption func(*JwtConfig)
+
+// WithCookieHttpOnly sets the HttpOnly flag for cookies
+func WithCookieHttpOnly(httpOnly bool) JwtOption {
+	return func(config *JwtConfig) {
+		config.CoookieHttpOnly = httpOnly
+	}
+}
+
+// WithCookieSecure sets the Secure flag for cookies
+func WithCookieSecure(secure bool) JwtOption {
+	return func(config *JwtConfig) {
+		config.CookieSecure = secure
+	}
+}
+
+// NewJwtConfig creates a new JwtConfig with the given secret and options
+func NewJwtConfig(secret string, opts ...JwtOption) *JwtConfig {
+	config := &JwtConfig{Secret: secret}
+
+	for _, opt := range opts {
+		opt(config)
+	}
+
+	return config
+}
