5e0cb595dfae3f3249a651f4e7b7762b4d747069
charlene0122 <zhifeiwu@hotmail.com>
2025-04-03 11:42:28 -0700

    get login_id from GetUserById query

--- pkg/mapper/mapperdb/query.sql
+++ pkg/mapper/mapperdb/query.sql
@@ -11,10 +11,10 @@ GROUP BY u.id, u.name, u.email, u.created_at, u.last_modified_at;
 
 -- name: GetUserById :one
 SELECT u.id, u.name, u.email, u.created_at, u.last_modified_at,
-       COALESCE(array_agg(r.name) FILTER (WHERE r.name IS NOT NULL), '{}') as roles
+       COALESCE(array_agg(r.name) FILTER (WHERE r.name IS NOT NULL), '{}') as roles, u.login_id
 FROM users u
 LEFT JOIN user_roles ur ON u.id = ur.user_id
 LEFT JOIN roles r ON ur.role_id = r.id
 WHERE u.id = $1
 AND u.deleted_at IS NULL
-GROUP BY u.id, u.name, u.email, u.created_at, u.last_modified_at;
+GROUP BY u.id, u.name, u.email, u.created_at, u.last_modified_at, u.login_id;
