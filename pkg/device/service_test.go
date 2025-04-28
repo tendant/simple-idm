ff3cc40a3aa792b2ca9b231915f902b0f0b8468c
charlene0122 <zhifeiwu@hotmail.com>
2025-04-28 15:52:08 -0700

    add service test

---
+++ pkg/device/service_test.go
@@ -0,0 +1,86 @@
+package device
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log/slog"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+func containerLog(ctx context.Context, container testcontainers.Container) {
+	// Retrieve logs
+	logs, err := container.Logs(ctx)
+	if err != nil {
+		slog.Error("Failed to get container logs:", "err", err)
+	}
+	defer logs.Close()
+
+	// Process and display logs
+	scanner := bufio.NewScanner(logs)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text()) // Print each log line
+	}
+
+	// Check for scanning errors
+	if err := scanner.Err(); err != nil {
+		slog.Error("Error reading logs", "err", err)
+	}
+}
+
+func setupTestDatabase(t *testing.T) (*pgxpool.Pool, func()) {
+	ctx := context.Background()
+
+	// Create PostgreSQL container
+	dbName := "idm_db"
+	dbUser := "idm"
+	dbPassword := "pwd"
+
+	container, err := postgres.Run(ctx,
+		"postgres:16-alpine",
+		postgres.WithInitScripts(filepath.Join("../../migrations", "idm_db.sql")),
+		// postgres.WithConfigFile(filepath.Join("testdata", "my-postgres.conf")),
+		postgres.WithDatabase(dbName),
+		postgres.WithUsername(dbUser),
+		postgres.WithPassword(dbPassword),
+		testcontainers.WithWaitStrategy(
+			wait.ForLog("database system is ready to accept connections").
+				WithOccurrence(2).
+				WithStartupTimeout(5*time.Second)),
+	)
+	require.NoError(t, err)
+	if err != nil {
+		slog.Error("Failed to start container:", "err", err)
+	}
+
+	// containerLog(ctx, container)
+
+	// Generate the connection string
+	connString, err := container.ConnectionString(ctx)
+	fmt.Println("Connection string:", connString)
+	require.NoError(t, err)
+
+	// Create connection pool
+	poolConfig, err := pgxpool.ParseConfig(connString)
+	require.NoError(t, err)
+
+	pool, err := pgxpool.NewWithConfig(ctx, poolConfig)
+	require.NoError(t, err)
+
+	cleanup := func() {
+		pool.Close()
+		if err := container.Terminate(ctx); err != nil {
+			t.Logf("failed to terminate container: %v", err)
+		}
+	}
+
+	return pool, cleanup
+}
