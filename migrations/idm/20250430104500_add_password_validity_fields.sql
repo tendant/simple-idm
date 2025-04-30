4bebf54ee5e5e591a39bcdb47b1dd0a197f5a959
charlene0122 <zhifeiwu@hotmail.com>
2025-04-30 16:41:13 -0700

    change password_expire_at to password_expires_at

--- migrations/idm/20250430104500_add_password_validity_fields.sql
+++ migrations/idm/20250430104500_add_password_validity_fields.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- +goose StatementBegin
 ALTER TABLE login ADD COLUMN password_updated_at TIMESTAMP WITHOUT TIME ZONE;
-ALTER TABLE login ADD COLUMN password_expire_at TIMESTAMP WITHOUT TIME ZONE;
+ALTER TABLE login ADD COLUMN password_expires_at TIMESTAMP WITHOUT TIME ZONE;
 ALTER TABLE login ADD COLUMN failed_login_attempts INTEGER DEFAULT 0;
 ALTER TABLE login ADD COLUMN locked_until TIMESTAMP WITHOUT TIME ZONE;
 ALTER TABLE login ADD COLUMN last_failed_attempt_at TIMESTAMP WITHOUT TIME ZONE;
@@ -9,7 +9,7 @@ ALTER TABLE login ADD COLUMN last_failed_attempt_at TIMESTAMP WITHOUT TIME ZONE;
 -- Set default values for existing records
 UPDATE login 
 SET password_updated_at = updated_at,
-    password_expire_at = updated_at + INTERVAL '90 days',
+    password_expires_at = updated_at + INTERVAL '90 days',
     failed_login_attempts = 0
 WHERE deleted_at IS NULL;
 -- +goose StatementEnd
@@ -17,7 +17,7 @@ WHERE deleted_at IS NULL;
 -- +goose Down
 -- +goose StatementBegin
 ALTER TABLE login DROP COLUMN IF EXISTS password_updated_at;
-ALTER TABLE login DROP COLUMN IF EXISTS password_expire_at;
+ALTER TABLE login DROP COLUMN IF EXISTS password_expires_at;
 ALTER TABLE login DROP COLUMN IF EXISTS failed_login_attempts;
 ALTER TABLE login DROP COLUMN IF EXISTS locked_until;
 ALTER TABLE login DROP COLUMN IF EXISTS last_failed_attempt_at;
