0763a72b5c1d4b230bdbe64f63b7c05273e85fb4
Lei Wang <tendant@gmail.com>
2025-10-26 22:49:26 -0700

    update frontend port to 4040

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     tailwindcss(),
   ],
   server: {
-    port: 3000,
+    port: 4040,
     proxy: {
       '/api/idm/auth': {
         target: 'http://localhost:4000',
